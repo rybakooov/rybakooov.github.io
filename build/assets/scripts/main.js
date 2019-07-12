12583ac2820dbc1e8497cce3bf60248f1ffa7db6
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-07-12 18:02:57 +0300

    pdy

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -7,4 +7,20 @@ $(document).ready(function(){
     /*баннер сайз*/
     $('.first-sec').height($('.first-sec').width() * k1);
   });
+
+  var $mission = $('.mission-wrap').masonry({
+    // options
+    itemSelector: '.mission-item'
+  });
+  
+  $mission.on( 'click', '.mission-item', function() {
+      $(this).find('.mission-item-invisible').css('height', '0px');
+    
+    $(this).toggleClass('mission-item_active');
+    
+      $(this).find('.mission-item-invisible').css('height', 'auto');
+    // trigger layout after item size changes
+    $mission.masonry('layout');
+  });
 })
+
