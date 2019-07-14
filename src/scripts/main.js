d617af0c74a84c9ea077effb1bfbf9e8883f07a4
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-07-14 20:35:14 +0300

    с пыра в ебасосину

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,18 +9,31 @@ $(document).ready(function(){
   });
 
   var $mission = $('.mission-wrap').masonry({
-    // options
     itemSelector: '.mission-item'
   });
   
   $mission.on( 'click', '.mission-item', function() {
-      $(this).find('.mission-item-invisible').css('height', '0px');
-    
+    $(this).find('.mission-item-invisible').css('height', '0px');
     $(this).toggleClass('mission-item_active');
-    
-      $(this).find('.mission-item-invisible').css('height', 'auto');
-    // trigger layout after item size changes
+    $(this).find('.mission-item-invisible').css('height', 'auto');
     $mission.masonry('layout');
+  });
+
+  $('.amen-links-item').click(function(){
+    if(!$(this).hasClass('amen-links-item_active')){
+      var number = $(this).attr('data-num');
+      $('.amen-links-item_active').removeClass('amen-links-item_active');
+      $('.amen-slide_active').removeClass('amen-slide_active');
+      $('.amen-slide[data-slide-number="' + number + '"]').addClass('amen-slide_active');
+      $('.amen-links-item[data-num="' + number + '"]').addClass('amen-links-item_active');
+    }
+  });
+  $('.cli-more-item__img').click(function(){
+    if(!$(this).parent().hasClass('cli-more-item_active')){
+      $('.cli-more-item_active').removeClass('cli-more-item_active');
+      $(this).parent().addClass('cli-more-item_active');
+    }
+
   });
 })
 
