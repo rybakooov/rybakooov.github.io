51fe12351d2c6b523cfe146fce80ceb6a637fbcd
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-09-07 01:30:25 +0300

    push1

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     }, 
     function(){
       $(this).children('.header-wrap-linklist-block').slideUp('fast');
-    });
+  });
 
   /*картинки*/
   let k1 = 605 / 1440;
@@ -109,7 +109,6 @@ $(document).ready(function(){
 
   /*plus minus*/
   $('.complectation-tr_more').each(function(){
-
     $(this).find('.complectation-tr-wrap-left').prepend('<svg width="10" height="10" xmlns="http://www.w3.org/2000/svg"><line class="line-svg" stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_2" y2="10" x2="5" y1="0" x1="5" stroke-width="1.5" stroke="#4479bc" fill="none"/><line stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_3" y2="5" x2="10" y1="5" x1="0" stroke-width="1.5" stroke="#4479bc" fill="none"/></svg>');
   });
 
@@ -118,7 +117,6 @@ $(document).ready(function(){
     $(this).find('.line-svg').toggleClass('line-svg_active');
   });
 
-  /*stages dots*/
 
 
   /******************************slick********************************/
@@ -169,15 +167,32 @@ $(document).ready(function(){
   })
 
 
-  
+  /*stages dots*/
   $('.stages-slider-dots').children('li').each(function(index){
     let html1 = '0' + $(this).find('button').html();
     $(this).find('button').html(html1);
     $('.stages-slider-item[data-slick-index=' + index + ']').find('.stages-slider-item-desc__num').html(html1);
   });
 
-
-
+  /* ********** contacts *********** */
+  $('.contacts-wrap-right-toggle__btn').click(function(){
+    if($(this).hasClass('contacts-wrap-right-toggle__btn_active')){ 
+      console.log('hu');
+    } else {
+      if($(this).hasClass('contacts-wrap-right-toggle__btn_l')) {
+        $('.contacts-wrap-right-main-block_active').toggleClass('contacts-wrap-right-main-block_active');
+        $('.contacts-wrap-right-main .contacts-wrap-right-main-block:first-child').toggleClass('contacts-wrap-right-main-block_active');
+        $('.contacts-wrap-right-toggle__btn_active').toggleClass('contacts-wrap-right-toggle__btn_active');
+        $(this).toggleClass('contacts-wrap-right-toggle__btn_active');
+      }
+      if($(this).hasClass('contacts-wrap-right-toggle__btn_r')){
+        $('.contacts-wrap-right-main-block_active').toggleClass('contacts-wrap-right-main-block_active');
+        $('.contacts-wrap-right-main .contacts-wrap-right-main-block:last-child').toggleClass('contacts-wrap-right-main-block_active');
+        $('.contacts-wrap-right-toggle__btn_active').toggleClass('contacts-wrap-right-toggle__btn_active');
+        $(this).toggleClass('contacts-wrap-right-toggle__btn_active');
+      }
+    }
+  });
 
   /*отключаем поиск на странице каталога*/
   $('.catalog').parent().parent().parent().find('.header-wrap-info-search').hide();
