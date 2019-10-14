b4e71a735c9245f5f563e83b7dbd86094651a527
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-10-14 17:03:06 +0300

    push

--- current/assets/scripts/main.js
+++ current/assets/scripts/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  $('.smile-faq-item-top').click(function(){
-    if(!$(this).closest('.smile-faq-item').hasClass('smile-faq-item_active')){
-      $(this).closest('.smile-faq-item').find('.smile-faq-item-bot').slideDown('fast');
-      $(this).closest('.smile-faq-item').toggleClass('smile-faq-item_active');
-    } else {
-      $(this).closest('.smile-faq-item').find('.smile-faq-item-bot').slideUp('fast');
-      $(this).closest('.smile-faq-item').toggleClass('smile-faq-item_active');
-    }
-  })
+  var goodTabContainers = $('.tasks-main');
+  goodTabContainers.hide().filter(':first').show();
+  $('.tasks-top a').click(function () {
+    goodTabContainers.hide();
+    goodTabContainers.filter(this.hash).show();
+    $('.tasks-top a').removeClass('selected');
+    $(this).addClass('selected');
+    return false;
+  }).filter(':first').click();
+  
 });
