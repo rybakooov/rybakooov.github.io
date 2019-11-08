63865cd598bf3574f6e09053ed3b1750ca99648d
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-11-08 17:20:00 +0300

    huyak

--- current/assets/scripts/main.js
+++ current/assets/scripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  /*слайдер1 на странице услуг*/
   if($('.amen-main-slider1').length) {
     $('.amen-main-slider1').slick({
       /*slidesToShow: 1,
@@ -6,6 +8,10 @@ $(document).ready(function(){
       arrows: false
     });
   }
+  /*слайдер1 на странице услуг конец*/
+
+
+  /*слайдер2 на странице услуг*/
   if($('.amen-main-slider2').length){
     $('.amen-main-slider2').slick({
       slidesToShow: 3,
@@ -16,10 +22,21 @@ $(document).ready(function(){
       asNavFor: '.amen-main-slider1'
     });
   }
+  /*слайдер2 на странице услуг конец*/
+
+
+  /*Поиск в шапке*/
   $(document).on('click', '.header-search', function () {
     if(!$('.header-linklist-search').hasClass('header-linklist-search_active')){
       $(this).css('visibility', 'hidden');
-      $(this).closest('.header').find('.header-linklist-search').fadeIn('fast').css('display', 'flex').addClass('header-linklist-search_active');
+      $(this).closest('.header').find('.header-linklist-search').fadeIn('slow').css('display', 'flex').addClass('header-linklist-search_active');
+      if($(window).width() < 1023 && $(window).width() > 767){
+        $('.header-contacts__link:nth-child(2)').fadeOut('fast');
+      }
+      if($(window).width() < 768){
+        $('.header-logo').fadeOut('fast');
+        $('.header-burger').css('margin-left', '-20px');
+      }
     }
   });
 
@@ -31,12 +48,22 @@ $(document).ready(function(){
         setTimeout(() => {
           div.removeClass('header-linklist-search_active');
         }, 200);
+
         $('.header-search').css('visibility', 'visible');
+        if($(window).width() < 1023 && $(window).width() > 767){
+          $('.header-contacts__link:nth-child(2)').fadeIn('fast');
+        }
+        if($(window).width() < 768){
+          $('.header-logo').fadeIn('fast');
+          $('.header-burger').css('margin-left', '20px');
+        }
       }
     });
   });
+  /*Поиск в шапке конец*/
 
 
+  /*селектор с ссылками услуги*/
   $(document).on('click', '.amen-aside__title', function () {
     $(this).closest('.amen-aside').find('.amen-aside-wrap').fadeToggle('fast');
     $(this).find('svg').toggleClass('arrow-top');
@@ -50,5 +77,21 @@ $(document).ready(function(){
       }
     });
   });
+  /*селектор с ссылками услуги конец*/
 
+  $(document).on('click', '.header-burger', function () {
+    $('.header-linklist-wrap').toggleClass('active');
+    $(this).toggleClass('active');
+  })
+
+
+  $(function($){
+    $(document).on('click touchend', function (e){
+      let div = $('.header-burger');
+      if ((!div.is(e.target) && div.has(e.target).length === 0) && (!$('.header-linklist-wrap').is(e.target) && $('.header-linklist-wrap').has(e.target).length === 0)) {
+        $('.header-linklist-wrap').removeClass('active');
+        div.removeClass('active');
+      }
+    });
+  });
 });
